3dfd458d4f6e9430c54e9e7c07c049b1e7ad12e8
Håvard Hellvik <havard.hellvik@mtllc.us>
2015-05-31 22:03:26 +0200

    Run checkout for all branches by creating local branches of the origins

--- VsoBackupTool/VsoBackup.Services/GitService.cs
+++ VsoBackupTool/VsoBackup.Services/GitService.cs
@@ -29,11 +29,35 @@ namespace VsoBackup.Services
 
         public void Clone(Value value, string path)
         {
-         _logger.WriteLog("Cloning repository '{0}'",value.name);
-         Repository.Clone(HttpUtility.UrlPathEncode(value.remoteUrl), path, new CloneOptions()
-          {
-               CredentialsProvider = CredentialsProvider
-          });
+            _logger.WriteLog("Cloning repository '{0}'", value.name);
+            var pathlocal = Repository.Clone(HttpUtility.UrlPathEncode(value.remoteUrl), path, new CloneOptions()
+             {
+                 CredentialsProvider = CredentialsProvider
+             });
+            var repo = new Repository(pathlocal);
+            foreach (var branch in repo.Branches)
+            {
+                string localName = branch.Name;
+                if (localName.Contains("origin/") && !localName.Contains("master"))
+                {
+                    try
+                    {
+                        localName = localName.Remove(0, 7);                       
+                        Branch newLocalBranch = repo.CreateBranch(localName, branch.Tip);
+                        // Make the local branch track the upstream one
+                        repo.Branches.Update(newLocalBranch,
+                             b => b.TrackedBranch = branch.CanonicalName);
+                        Branch trackingBranch = repo.Branches[localName];
+                        repo.Checkout(trackingBranch);
+                    }
+                    catch(System.Exception ex)
+                    {
+                        //Gets exception of some repos when trying to create an existing branch..
+                        //NameConflictException
+                    }
+                }
+            }
+
         }
     }
 }
